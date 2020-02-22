8d386dbb55d3bc699912748a579b0370f60041e2
Monica Luo <lalaphoon@gmail.com>
2020-02-21 23:03:02 -0500

    Added login and register mock api

---
+++ src/main/java/com/delivery/Register.java
@@ -0,0 +1,30 @@
+package com.delivery;
+
+import org.json.JSONObject;
+import rpc.RpcHelper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class Register extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        //TODO: read body and fetch the correct user_id
+        JSONObject obj = new JSONObject();
+        obj.put("user_id", "1111");
+        RpcHelper.writeJsonObject(response, obj);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        //TODO: read body and fetch the correct user_id
+        JSONObject obj = new JSONObject();
+        obj.put("user_id", "1111");
+        RpcHelper.writeJsonObject(response, obj);
+    }
+}

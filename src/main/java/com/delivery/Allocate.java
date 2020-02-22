b26d88b8473b046920798f15a576f74660807015
Monica Luo <lalaphoon@gmail.com>
2020-02-22 00:24:56 -0500

    Created a sample mock api for POST /allocate

--- src/main/java/com/delivery/Allocate.java
+++ src/main/java/com/delivery/Allocate.java
@@ -15,7 +15,29 @@ import java.io.IOException;
 @WebServlet("/allocate")
 public class Allocate extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //TODO: fix it to be real
+        JSONArray array = new JSONArray();
+        JSONObject obj = new JSONObject();
+        try {
+            array.put(new JSONObject()
+                    .put("route_id","RT_SASjsdf23j")
+                    .put("price" , 15.6)
+                    .put("route", 3)
+                    .put("deliver_type", "DRONE")
+            );
+            array.put(new JSONObject()
+                    .put("route_id","RT_lsjdf242ew")
+                    .put("price" , 14.2)
+                    .put("route", 3)
+                    .put("deliver_type", "ROBOT")
 
+            );
+
+            obj.put("routes", array);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RpcHelper.writeJsonObject(response, obj);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

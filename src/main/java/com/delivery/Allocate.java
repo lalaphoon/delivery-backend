1606afafa02e7de3d9c2ce42ff3bf5da8d2de232
Monica Luo <lalaphoon@gmail.com>
2020-02-28 14:03:41 -0500

    Added allocationg mock api and fix the negative sign for lon

--- src/main/java/com/delivery/Allocate.java
+++ src/main/java/com/delivery/Allocate.java
@@ -19,22 +19,45 @@ public class Allocate extends HttpServlet {
         JSONArray array = new JSONArray();
         JSONObject obj = new JSONObject();
         try {
+            JSONArray route1 = new JSONArray();
+            route1.put(new JSONObject().put("lat",37.7571 ).put("lon", -122.4866));
+            route1.put(new JSONObject().put("lat",37.777630).put("lon", -122.496440));
+            route1.put(new JSONObject().put("lat",37.777340 ).put("lon", -122.410350));
+            route1.put(new JSONObject().put("lat",37.792 ).put("lon", -122.4052 ));
+
+            JSONArray route2 = new JSONArray();
+            route2.put(new JSONObject().put("lat",37.7571 ).put("lon", -122.4866));
+            route2.put(new JSONObject().put("lat",37.792 ).put("lon", -122.4052 ));
+
+            JSONArray marker = new JSONArray();
+            marker.put(new JSONObject().put("marker_name", "Starting").put("location", new JSONObject().put("lat", 37.777630).put("lon", -122.496440)));
+            marker.put(new JSONObject().put("marker_name", "Destination").put("location", new JSONObject().put("lat", 37.777340).put("lon", -122.410350)));
+
+
             array.put(new JSONObject()
                     .put("route_id","RT_SASjsdf23j")
-                    .put("price" , 15.6)
-                    .put("route", 3)
+                    .put("price" , 98.6)
                     .put("deliver_type", "DRONE")
+                    .put("available_time", "2019-04-24 18:00:00")
+                    .put("usage_time", "00:15:00")
+                    .put("distance", 534.1)
+                    .put("route", route1)
             );
+
+
             array.put(new JSONObject()
                     .put("route_id","RT_lsjdf242ew")
-                    .put("price" , 14.2)
-                    .put("route", 3)
+                    .put("price" , 54.2)
                     .put("deliver_type", "ROBOT")
-
+                    .put("available_time", "2019-04-24 18:00:00")
+                    .put("usage_time", "01:01:12")
+                    .put("distance", 1534.1)
+                    .put("route", route2)
             );
 
             obj.put("routes", array);
             obj.put("order_id", "OR_1234");
+            obj.put("markers", marker);
         } catch (JSONException e) {
             e.printStackTrace();
         }

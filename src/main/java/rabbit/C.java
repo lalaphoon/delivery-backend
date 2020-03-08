af4a7e25fead35fa84d4968d18f57629f7088294
Monica Luo <lalaphoon@gmail.com>
2020-03-08 19:17:35 -0400

    Added multitask into a queue.

--- src/main/java/rabbit/C.java
+++ src/main/java/rabbit/C.java
@@ -4,7 +4,7 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 
 public class C {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         // 创建连接工厂
@@ -12,21 +12,40 @@ public class C {
 //		设置RabbitMQ地址
         factory.setHost("localhost");
 //		创建一个新的连接
-        Connection connection = factory.newConnection();
+        final Connection connection = factory.newConnection();
 //		创建一个频道
-        Channel channel = connection.createChannel();
+        final Channel channel = connection.createChannel();
+        boolean durable = true;
 //		声明要关注的队列 -- 在RabbitMQ中，队列声明是幂等性的（一个幂等操作的特点是其任意多次执行所产生的影响均与一次执行的影响相同），也就是说，如果不存在，就创建，如果存在，不会对已经存在的队列产生任何影响。
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         System.out.println("C [*] Waiting for messages. To exit press CTRL+C");
 //		DefaultConsumer类实现了Consumer接口，通过传入一个频道，告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery
-        Consumer consumer = new DefaultConsumer(channel) {
+
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println("C [x] Received '" + message + "'");
+                System.out.println("Worker1 [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println("Worker1 [x] Done");
+                    // 消息处理完成确认
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
             }
         };
 //		自动回复队列应答 -- RabbitMQ中的消息确认机制，后面章节会详细讲解
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        try {
+            Thread.sleep(1000); // 暂停1秒钟
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
